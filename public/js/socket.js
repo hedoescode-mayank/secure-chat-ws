d9bb5cea62b9cb6974f20b343b4c8ccfe4b07f06
hedoescode-mayank <222556104+hedoescode-mayank@users.noreply.github.com>
2026-05-06 00:11:08 +0530

    Created public/js/socket.js: Implemented room-aware WebSocket client

---
+++ public/js/socket.js
@@ -0,0 +1,49 @@
+class SocketClient {
+  constructor(url) {
+    this.url = url;
+    this.socket = null;
+    this.onMessageCallback = null;
+    this.onOpenCallback = null;
+    this.onCloseCallback = null;
+  }
+
+  connect() {
+    this.socket = new WebSocket(this.url);
+
+    this.socket.onopen = () => {
+      console.log("Connected to server");
+      if (this.onOpenCallback) this.onOpenCallback();
+    };
+
+    this.socket.onmessage = (event) => {
+      if (this.onMessageCallback) {
+        this.onMessageCallback(JSON.parse(event.data));
+      }
+    };
+
+    this.socket.onclose = () => {
+      console.log("Disconnected from server");
+      if (this.onCloseCallback) this.onCloseCallback();
+    };
+
+    this.socket.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+  }
+
+  send(type, payload) {
+    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+      this.socket.send(JSON.stringify({ type, ...payload }));
+    }
+  }
+
+  joinRoom(roomId) {
+    this.send("join", { roomId });
+  }
+
+  sendChat(data, iv) {
+    this.send("chat", { data, iv });
+  }
+}
+
+window.SocketClient = SocketClient;
