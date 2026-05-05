ea78ab8ad26c5da23f3631fad67f743886805be9
hedoescode-mayank <222556104+hedoescode-mayank@users.noreply.github.com>
2026-05-06 00:11:07 +0530

    Created config/constants.js: Defined shared configuration and color tokens

---
+++ config/constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+  PORT: process.env.PORT || 4000,
+  MAX_ROOM_SIZE: 10,
+  SYSTEM_NAME: "SecureChat-OS",
+  VERSION: "2.0.0",
+  COLORS: {
+    SUCCESS: "\u001b[32m",
+    ERROR: "\u001b[31m",
+    INFO: "\u001b[34m",
+    RESET: "\u001b[0m"
+  }
+};
